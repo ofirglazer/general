ef4c7d56194d1e87d03c1f2aeb66bf9eb812c7d0
ofirglazer <ofirglazer@gmail.com>
2024-12-06 22:38:16 +0200

    draw rect at mouth click

---
+++ draw graphics.py
@@ -0,0 +1,57 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+GRAY = (127, 127, 127)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+width = 640
+length = 320
+screen = pygame.display.set_mode((width, length))
+
+background = GRAY
+
+running = True
+drawing = False
+rect_start = (0, 0)
+rect_size = (0, 0)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        elif event.type == MOUSEBUTTONDOWN:
+            rect_start = event.pos
+            drawing = True
+        elif event.type == MOUSEBUTTONUP:
+            rect_end = event.pos
+            rect_size = (rect_end[0] - rect_start[0], rect_end[1] - rect_start[1])
+            drawing = False
+        elif event.type == MOUSEMOTION and drawing == True:
+            rect_end = event.pos
+            rect_size = (rect_end[0] - rect_start[0], rect_end[1] - rect_start[1])
+
+    screen.fill(background)
+    pygame.draw.rect(screen, RED, (rect_start, rect_size), 2)
+
+    # pygame.draw.rect(screen, RED, (50, 20, 12, 100))
+    # pygame.draw.rect(screen, BLUE, (150, 100, 120, 100))
+    # pygame.draw.rect(screen, GREEN, (100, 60, 120, 200))
+    # pygame.draw.rect(screen, RED, (350, 20, 120, 100), 1)
+    # pygame.draw.rect(screen, GREEN, (400, 60, 120, 100), 4)
+    # pygame.draw.rect(screen, BLUE, (450, 100, 120, 100), 8)
+
+    # pygame.draw.ellipse(screen, RED, (50, 20, 100, 100))
+    # pygame.draw.ellipse(screen, GREEN, (100, 60, 130, 100))
+    # pygame.draw.ellipse(screen, BLUE, (150, 100, 160, 100))
+    # pygame.draw.ellipse(screen, RED, (350, 20, 160, 100), 1)
+    # pygame.draw.ellipse(screen, GREEN, (400, 60, 160, 100), 4)
+    # pygame.draw.ellipse(screen, BLUE, (450, 100, 160, 100), 8)
+
+    pygame.display.update()
+
+
+pygame.quit()
